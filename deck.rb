aa328f981ff1ceb544d99856c5259796d0a61e43
Tim Shadel <tim@shadelsoftware.com>
2015-07-06 20:30:57 -0600

    Try out a horizontal fill.

--- deck.rb
+++ deck.rb
@@ -103,6 +103,24 @@ module Steps
     end
   end
 
+  module HorizontalFill
+    def hfill pile
+      0.upto(pile) do |n|
+        count = top_of_pile_face(n) - 1
+        if (count > @discards[n].count)
+          discard n
+        end
+      end
+    end
+
+    def unhfill pile
+      count = @discards[pile].count - 1
+      count.times do
+        undiscard pile
+      end
+    end
+  end
+
   module InvertedCountCut
     def inverted_count_cut length
       top = @cards.slice! 0, length
@@ -127,6 +145,7 @@ class Deck
   include Steps::Discard
   include Steps::Gather
   include Steps::DiscardFill
+  include Steps::HorizontalFill
   include Steps::InvertedCountCut
 
   NAMES = %w{AC 2C 3C 4C 5C 6C 7C 8C 9C TC JC QC KC AD 2D 3D 4D 5D 6D 7D 8D 9D TD JD QD KD AH 2H 3H 4H 5H 6H 7H 8H 9H TH JH QH KH AS 2S 3S 4S 5S 6S 7S 8S 9S TS JS QS KS}
@@ -164,7 +183,7 @@ class Deck
   end
 
   def top_of_pile index
-    value = @discards[index].first
+    value = @discards[index].last
     value = 53 if value == 'A' || value == 'B'
     value
   end
